ea5693e839e91d82e9eb7c0a16bca05610be5228
UJHa <ddkk94@naver.com>
2023-11-29 03:41:05 +0900

    countDown 기능 추가

---
+++ Source/RealGameProject/CountDown.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+//#include "Components/TextRenderComponent.h"
+#include "CountDown.h"
+
+// Sets default values
+ACountDown::ACountDown()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	CountdownText = CreateDefaultSubobject<UTextRenderComponent>(TEXT("CountdownNumber"));
+	CountdownText->SetHorizontalAlignment(EHTA_Center);
+	CountdownText->SetWorldSize(150.0f);
+	RootComponent = CountdownText;
+	CountdownTime = 3;
+}
+
+// Called when the game starts or when spawned
+void ACountDown::BeginPlay()
+{
+	Super::BeginPlay();
+	UpdateTimerDisplay();
+	GetWorldTimerManager().SetTimer(CountdownTimerHandle, this, &ACountDown::AdvanceTimer, 1.0f, true);
+}
+
+// Called every frame
+void ACountDown::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
+void ACountDown::UpdateTimerDisplay()
+{
+	CountdownText->SetText(FString::FromInt(FMath::Max(CountdownTime, 0)));
+}
+
+void ACountDown::AdvanceTimer()
+{
+	--CountdownTime;
+	UpdateTimerDisplay();
+	if (CountdownTime < 1)
+	{
+		GetWorldTimerManager().ClearTimer(CountdownTimerHandle);
+		CountdownHasFinihsed();
+	}
+}
+
+void ACountDown::CountdownHasFinished()
+{
+	
+}
+
+void ACountDown::CountdownHasFinished_Implementation()
+{
+	CountdownText->SetText(TEXT("GO!"));
+}
---
+++ Source/RealGameProject/CountDown.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/TextRenderComponent.h"
+#include "CountDown.generated.h"
+
+UCLASS()
+class REALGAMEPROJECT_API ACountDown : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACountDown();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	//초 단위의 카운트다운 실행 시간입니다.
+	UPROPERTY(EditAnywhere)
+	int32 CountdownTime;
+	UTextRenderComponent* CountdownText;
+	void UpdateTimerDisplay();
+
+	void AdvanceTimer();
+
+	void CountdownHasFinihsed();
+
+
+	UFUNCTION(BlueprintNativeEvent)
+	void CountdownHasFinished();
+	virtual void CountdownHasFinished_Implementation();
+
+	FTimerHandle CountdownTimerHandle;
+};
