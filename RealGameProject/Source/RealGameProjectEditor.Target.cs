c4336d58d1ea55fa666e4a04629649990c89ccb8
UJHa <ddkk94@naver.com>
2023-11-28 19:12:55 +0900

    프로젝트 설정 초기화, gitignore 갱신

--- RealGameProject/Source/RealGameProjectEditor.Target.cs
+++ RealGameProject/Source/RealGameProjectEditor.Target.cs
@@ -9,7 +9,6 @@ public class RealGameProjectEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
-		ExtraModuleNames.Add("RealGameProject");
+		ExtraModuleNames.AddRange( new string[] { "RealGameProject" } );
 	}
 }
